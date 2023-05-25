9a9d305dba53a95dc0bb0fa79690a81ba6419b73
MaldifassiJacopo <maldifassi.jacopo@iisgalvanimi.edu.it>
2023-05-25 12:35:28 +0000

    verifica a terminata si va a berlino beppe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SecondiComponent } from './secondi/secondi.component';
+import { PrimiComponent } from './primi/primi.component';
+const routes: Routes = [
+  { path: 'primi', component: PrimiComponent},
+  { path: 'secondi', component: SecondiComponent},
+  { path: '', redirectTo: '/secondi', pathMatch:'full'}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
